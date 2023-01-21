1fe06eca7be99271f8b1a53e2cc4471a33df56ec
Salah Kouhen <kouhen@OXAOPPWTLAP33.physics.ox.ac.uk>
2023-01-21 13:17:29 +0000

    Created quest class

---
+++ app/src/main/java/com/salahkouhen/quests/Quest.java
@@ -0,0 +1,50 @@
+package com.salahkouhen.quests;
+
+public class Quest {
+
+    private String title;
+    private String description;
+    private String reward;
+    private int value; // 0 - platinum 1 - gold 2 - silver 3 - copper
+    private boolean recurring; // does quest disappear after press complete
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+
+    public void setReward(String reward) {
+        this.reward = reward;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isRecurring() {
+        return recurring;
+    }
+
+    public void setRecurring(boolean recurring) {
+        this.recurring = recurring;
+    }
+}
